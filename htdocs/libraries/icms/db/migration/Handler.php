31c4764cc8aec144fea822ca8995bd961ea21b88
Raimondas Rimkevičius <github@mekdrop.name>
2015-12-15 19:06:31 +0200

    SomeGetObject fixes

--- htdocs/libraries/icms/db/migration/Handler.php
+++ htdocs/libraries/icms/db/migration/Handler.php
@@ -20,17 +20,27 @@ class icms_db_migration_Handler extends icms_core_ObjectHandler {
 	 */
 	public function &create() {
 		$obj = new \icms_db_migration_Object();
-		$obj->setVar('id', time());
-		$cwd = getcwd();
+		$obj->setVar('id', time());		
+		$obj->setVar('module', $this->getModuleNameFromPath(getcwd()));		
+		$obj->setNew();		
+		return $obj;
+	}
+	
+	/**
+	 * Get module name from path
+	 * 
+	 * @param string		$cwd		Path from where to extract module name
+	 * 
+	 * @return string|null
+	 */
+	protected function getModuleNameFromPath($cwd) {
 		$o = strrpos($cwd, ICMS_MODULES_PATH, -strlen($cwd));
 		if ($o !== FALSE) {
-			$module_path_name = substr($cwd, strrpos($cwd, DIRECTORY_SEPARATOR, $o + 1));
-			$obj->setVar('module', $module_path_name);
+			return substr($cwd, strrpos($cwd, DIRECTORY_SEPARATOR, $o + 1));
 		} elseif (strrpos($cwd, ICMS_ROOT_PATH . DIRECTORY_SEPARATOR . 'install', -strlen($cwd)) !== false) {
-			$obj->setVar('module', '@install');
-		}		
-		$obj->setNew();
-		return $obj;
+			return '@install';
+		}
+		return null;
 	}
 
 	/**
@@ -43,36 +53,121 @@ class icms_db_migration_Handler extends icms_core_ObjectHandler {
 		$criteria->setLimit(1);
 		$objs = $this->getObjects($criteria, false);
 		return (isset($objs[0]) === true)?$objs[0]:($ret = null);
-	}
+	}	
 	
     /**
      * Retrieve objects from the database
      *
-     * @param object $criteria {@link icms_db_criteria_Element} conditions to be met
-     * @param bool $id_as_key use the ID as key for the array?
+     * @param icms_db_criteria_Element	$criteria	Conditions to be met
+     * @param bool						$id_as_key	Use the ID as key for the array?
      *
      * @return array
      */
     public function getObjects($criteria = null, $id_as_key = false) {
-		
-	}	
+		$ret = [];
+		if ($criteria instanceof \icms_db_criteria_Element) {
+			if ($criteria->groupby) {
+				throw new Exception('Grouby criteria definition for icms_db_migration_Handler currently is unsuported');
+			}			
+			if (($criteria instanceof \icms_db_criteria_Item) && isset($criteria->function)) {
+				throw new Exception('Grouby criteria definition for icms_db_migration_Handler currently is unsuported');
+			}
+			if ($criteria->limit > 0) {
+				$limit = $criteria->limit;
+				$o = 0;
+			}
+			if ($criteria->start > 0) {
+				$start = $criteria->start;
+				$i = 0;
+			}			
+			$code = $criteria->renderPHP();
+			if (!empty($code)) {
+				$filter_func = eval('return function($data) { return ' . $code . ';}');
+			}
+		}
+		foreach ([
+			ICMS_MODULES_PATH . DIRECTORY_SEPARATOR . '*' . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . '*_*.php',
+			ICMS_ROOT_PATH . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . '*_*.php'
+		] as $path) {
+			$iterator = new \GlobIterator(
+				$path,
+				\FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::CURRENT_AS_FILEINFO
+			);
+			foreach ($iterator as $fileInfo) {				
+				$obj = $this->convertFileInfoToObject($fileInfo);
+				if (
+						(isset($filter_func) && !$filter_func($obj)) ||
+						(isset($start) && ($i++ < $start))
+				) {
+					continue;
+				}
+				if ($id_as_key === true) {
+					$ret[$obj->id] = $obj;
+				} else {
+					$ret[] = $obj;
+				}
+				if (isset($limit) && (++$o > $limit)) {
+					break;
+				}
+			}
+		}
+		return $ret;
+	}
+
+	/**
+	 * Converts SplFileInfo file system object to \icms_db_migration_Object
+	 * 
+	 * @param \SplFileInfo					$info
+	 * 
+	 * @return \icms_db_migration_Object
+	 */
+	protected function convertFileInfoToObject(\SplFileInfo $info) {
+		$obj = new \icms_db_migration_Object();
+		$obj->unsetNew();
+		list($id, $name) = explode('_', $info->getBasename('.php'), 2);
+		$module = $this->getModuleNameFromPath($info->getPath());
+		$obj->assignVars(compact('module', 'id', 'name'));
+		return $obj;
+	}
 
 	/**
 	 * insert/update object
 	 *
 	 * @param object $object
 	 */
-	public function insert(&$object) {
+	public function insert(\icms_db_migration_Object &$object) {
+		if ($object->isChanged()) {
+			
+		}
+	}
+	
+	public function apply(\icms_db_migration_Object &$object) {
 		
 	}
+	
+	public function revert(\icms_db_migration_Object &$object) {
+		
+	}	
 
 	/**
-	 * delete object from database
-	 *
-	 * @param object $object
+	 * Delete migration
+	 * 
+	 * @param \icms_db_migration_Object $object		Migration to delete
+	 * 
+	 * @return boolean
 	 */
-	public function delete(&$object) {
-		
-	}
+	public function delete(\icms_db_migration_Object &$object) {
+		if ($object->isNew()) {
+			return false;
+		}
+		if ($object->applied) {
+			try {
+				$this->revert($object);
+			} catch (Exception $ex) {
+				return false;
+			}
+		}		
+		return \icms_core_Filesystem::deleteFile($object->getFileName());		
+	}	
 	
 }
--- htdocs/libraries/icms/db/migration/Object.php
+++ htdocs/libraries/icms/db/migration/Object.php
@@ -8,11 +8,19 @@
 /**
  * This migration object
  *
- * @author Raimondas Rimkevičius aka MekDrop <mekdrop@impresscms.org>
+ * @author	Raimondas Rimkevičius aka MekDrop <mekdrop@impresscms.org>
  * @package ImpressCMS\Database\Migration
+ * 
+ * @property int	$id			ID of this migration
+ * @property string	$name		Name of migration
+ * @property string $module		Module where this migration belongs
+ * @property bool	$applied	Was applied?
  */
 class icms_db_migration_Object extends icms_core_Object {
 		
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
 		parent::initVar('id', self::DTYPE_INTEGER, null, true);
 		parent::initVar('name', self::DTYPE_STRING, null, true, 200, [
@@ -21,10 +29,23 @@ class icms_db_migration_Object extends icms_core_Object {
 		parent::initVar('module', self::DTYPE_STRING, null, true, 255, [
 			self::VARCFG_VALIDATE_RULE => '/(@install|[A-Za-z0-9_\ \.]+)/i',
 		]);
+		parent::initVar('applied', self::DTYPE_BOOLEAN, false, true);
 	}
 	
-	public function exec() {
-		
+	/**
+	 * Gets filename for this migration
+	 * 
+	 * @return string
+	 */
+	public function getFileName() {
+		if (!$this->id || !$this->name || !$this->module) {
+			throw new Exception('All required data not set for filename generation');
+		}
+		if ($this->module === '@install') {
+			return ICMS_ROOT_PATH . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . $this->id . '_' . $this->name . '.php';
+		} else {
+			return ICMS_MODULES_PATH . DIRECTORY_SEPARATOR . $this->module . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . $this->id . '_' . $this->name . '.php';
+		}
 	}
 	
 }
