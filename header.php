d1b6f06ebea2f6add6fe59996e6b3b102cf0b6ee
Raimondas Rimkevičius <github@mekdrop.name>
2019-10-20 01:02:50 +0300

    some more things visible

--- header.php
+++ header.php
@@ -32,13 +32,3 @@
  *
  * @todo        Remove this file in the future
  */
-\icms::$logger->stopTime('Module init');
-\icms::$logger->startTime('ICMS output init');
-
-global $xoopsOption;
-$xoopsOption['theme_use_smarty'] = 1;
-
-\icms::$response = new \icms_response_HTML($xoopsOption);
-
-\icms::$logger->stopTime('ICMS output init');
-\icms::$logger->startTime('Module display');
