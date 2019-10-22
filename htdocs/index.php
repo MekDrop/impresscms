59955f45cde86891afffd7cb281d274a0b7a92dc
Raimondas Rimkevičius <github@mekdrop.name>
2019-10-23 01:51:33 +0300

    error handling with whoops

--- htdocs/index.php
+++ htdocs/index.php
@@ -16,8 +16,38 @@ include_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'mainfile.php';
  * @var \League\Route\Router $router
  */
 $router = \Imponeer\ComposerRoutesRegistrationPlugin\PredefinedRouterFactory::create();
+
+switch ($GLOBALS['icmsConfig']['debug_mode']) {
+	case 2:
+		$whoopsHandler = new \Whoops\Handler\PrettyPageHandler();
+		break;
+	case 1:
+
+		break;
+	default:
+		$whoopsHandler = new \Whoops\Handler\CallbackHandler(
+			function ($exception, \Whoops\Exception\Inspector $inspector, $run) {
+				switch ($inspector->getExceptionName()) {
+					case \League\Route\Http\Exception\NotFoundException::class:
+						echo 'Page not found';
+						break;
+					default:
+						echo 'System error';
+				}
+			}
+		);
+}
+
+if (isset($whoopsHandler)) {
+	$router->middleware(new \Middlewares\Whoops(
+		(new \Whoops\Run())
+			->prependHandler($whoopsHandler)
+			->register()
+	));
+}
+
 $request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
 
-(new \Zend\HttpHandlerRunner\Emitter\SapiEmitter())->emit(
-	$router->dispatch($request)
-);
+$response = $router->dispatch($request);
+
+(new \Zend\HttpHandlerRunner\Emitter\SapiEmitter())->emit($response);
